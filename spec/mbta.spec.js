a968fb66735916c0c233ad4bd69708575fb1ac89
jordanallain <jordan.allain@generalassemb.ly>
2018-07-24 13:59:33 -0400

    Improve messaging displayed when running tests
    - Improve the messages for test output to aid developers understanding.
    - Closes #282

--- spec/mbta.spec.js
+++ spec/mbta.spec.js
@@ -7,67 +7,67 @@ const mbta = require('../lib/mbta.js')
 
 describe('mbta', () => {
   describe('Red Line', () => {
-    it('goes from "Alewife" to "South Station"', () => {
+    it('trip from "Red" line "Alewife" station to "Red" line "South Station"', () => {
       expect(mbta.stopsBetweenStations('Red', 'Alewife', 'Red', 'South Station')).to.equal(7)
     })
 
-    it('goes from "South Station" to "Alewife"', () => {
+    it('trip from "Red" line "South Station" to "Red" line "Alewife" station', () => {
       expect(mbta.stopsBetweenStations('Red', 'South Station', 'Red', 'Alewife')).to.equal(7)
     })
   })
 
   describe('Green Line', () => {
-    it('goes from "Haymarket" to "Copley"', () => {
+    it('trip from "Green" line "Haymarket" station to "Green" line "Copley" station', () => {
       expect(mbta.stopsBetweenStations('Green', 'Government Center',
         'Green', 'Kenmore')).to.equal(6)
     })
 
-    it('goes from "Copley" to "Haymarket"', () => {
+    it('trip from from "Green" line "Copley" station to "Green" line "Haymarket" station', () => {
       expect(mbta.stopsBetweenStations('Green', 'Kenmore',
         'Green', 'Government Center')).to.equal(6)
     })
   })
 
   describe('Orange Line', () => {
-    it('goes from "North Station" to "Forest Hills"', () => {
+    it('trip from "Orange" line "North Station" to "Orange" line "Forest Hills" station', () => {
       expect(mbta.stopsBetweenStations('Orange', 'North Station', 'Orange',
-                                       'Forest Hills')).to.equal(7)
+        'Forest Hills')).to.equal(7)
     })
 
-    it('goes from "Forest Hills" to "North Station"', () => {
+    it('trip from "Orange" line "Forest Hills" station to "Orange" line "North Station"', () => {
       expect(mbta.stopsBetweenStations('Orange', 'Forest Hills', 'Orange',
-                                       'North Station')).to.equal(7)
+        'North Station')).to.equal(7)
     })
   })
 
   describe('Red and Green Lines', () => {
-    it('goes from "South Station" to "Kenmore"', () => {
+    it('trip from "Red" line "South Station" to "Green" line "Kenmore" station', () => {
       expect(mbta.stopsBetweenStations('Red', 'South Station', 'Green', 'Kenmore')).to.equal(6)
     })
 
-    it('goes from "Government Center" to "Alewife"', () => {
+    it('trip from "Green" line "Government Center" station to "Red" line "Alewife" station', () => {
       expect(mbta.stopsBetweenStations('Green', 'Government Center', 'Red', 'Alewife')).to.equal(7)
     })
   })
 
   describe('Red and Orange Lines', () => {
-    it('goes from "South Station" to "Forest Hills"', () => {
+    it('trip from "Red" line "South Station" to "Orange" line "Forest Hills" station', () => {
       expect(mbta.stopsBetweenStations('Red', 'South Station',
         'Orange', 'Forest Hills')).to.equal(6)
     })
 
-    it('goes from "North Station" to "Alewife"', () => {
+    it('trip from "Orange" line "North Station" to "Red" line "Alewife" station', () => {
       expect(mbta.stopsBetweenStations('Orange', 'North Station', 'Red', 'Alewife')).to.equal(8)
     })
   })
 
   describe('Green and Orange Lines', () => {
-    it('goes from "Government Center" to "Forest Hills"', () => {
+    it('trip from "Green" line "Government Center" station to "Orange" line "Forest Hills" station', () => {
       expect(mbta.stopsBetweenStations('Green', 'Government Center', 'Orange',
-                                       'Forest Hills')).to.equal(6)
+        'Forest Hills')).to.equal(6)
     })
 
-    it('goes from "North Station" to "Kenmore"', () => {
+    it('trip from "Orange" line "North Station" to "Green" line "Kenmore" station', () => {
       expect(mbta.stopsBetweenStations('Orange', 'North Station', 'Green', 'Kenmore')).to.equal(7)
     })
   })
